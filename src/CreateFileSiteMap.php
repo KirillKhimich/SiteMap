0486ffd605b7a57d8e48aa99bc6a7171cf25bd22
Kirill <kirillposp1973@gmail.com>
2022-07-21 15:29:05 +0700

    v 1.2.1

--- src/CreateFileSiteMap.php
+++ src/CreateFileSiteMap.php
@@ -2,6 +2,7 @@
 
 namespace Kirillkhimich\Sitemap;
 
+
 class CreateFileSiteMap
 {
     private $dir;
@@ -10,9 +11,8 @@ class CreateFileSiteMap
     }
 
     public function CreateFile($type,$path = ""){
-
         try {
-            $this->CreateDir($this->dir,$path);
+            $createdir = $this->CreateDir($this->dir,$path);
         }catch (\Exception $e){
             die($e->getMessage());
         }
@@ -27,26 +27,28 @@ class CreateFileSiteMap
             }else{
                 throw new \Exception("Ошибка записи файла:проверьте переданный параметр типа");
             }
-            $file = $this->dir ."/". "sitemap". $typeFile;
-                if (!file_exists($file)){
-                    $stream = fopen($file, "w");
-                    fclose($stream);
-                }
+            $file = $createdir ."/". "sitemap". $typeFile;
+            if (!file_exists($file)){
+                $stream = fopen($file, "w");
+                fclose($stream);
+            }
             return $file;
         }else throw new \Exception("Ошибка записи файла:недостаточно прав на создание файла");
     }
 
     private function CreateDir($dir,$path = ""){
         $rights = substr(sprintf('%o', fileperms($_SERVER['DOCUMENT_ROOT'])), -4);
+        if (is_string($path)){
+            if (!empty($path)){
+                $dir = $this->dir . "/$path";
+            }else{
+                $dir = $this->dir . "/sitemaps";
+            }
+        }else throw new \Exception("Ошибка записи файла:путь должен быть строкой");
         if (!file_exists($dir)){
             if ($rights[1] >= 6) {
-                if (empty($path)){
-                    $dir = $this->dir . "/sitemaps";
-                    mkdir($dir);
-                }else{
-                    $dir = $this->dir . "/$path";
-                    mkdir($dir);
-                }
+                mkdir($dir);
+                return $dir;
             }else throw new \Exception("Ошибка записи файла: недостаточно прав на создание папки");
         }
     }
